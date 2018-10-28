4d1cfdf8320b78f4c6942bed043fd23f83a76d93
rose <714465>
2018-10-28 18:30:19 +0800

    excel工具

--- smalltools/src/main/java/excell/ExcelUtil.java
+++ smalltools/src/main/java/excell/ExcelUtil.java
@@ -4,8 +4,6 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -18,47 +16,56 @@ import java.util.List;
  * @author gyc
  * @date 2018/10/26  21:45
  */
-public class ExcelUtil {
-    public static <T> void setExcel(String title,String[] columnNames, List<T> tList) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, IOException {
+public class ExcelUtil<T> {
 
+    public HSSFWorkbook setExcel(String title,String[] columnNames, List<T> tList) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, IOException {
 //        1.创建Excel工作薄对象
         HSSFWorkbook workbook=new HSSFWorkbook();
 //        2.创建Excel工作表对象
         HSSFSheet sheet=workbook.createSheet(title);
-//        3.创建Excel工作表的第一行
         HSSFRow row=null;
+//        3.创建Excel工作表的第一行，并填充列名
         row=sheet.createRow(0);
         for(int i=0;i<columnNames.length;i++){
             row.createCell(i).setCellValue(columnNames[i]);
         }
+        Field[] declaredFields = tList.get(0).getClass().getDeclaredFields();
+//        4.将数据填充至表格中
         for(int j=1;j<=tList.size();j++){
             T t= tList.get(j-1);
-            Field[] declaredFields = t.getClass().getDeclaredFields();
             row=sheet.createRow(j);
             for(int i=0;i<declaredFields.length;i++){
+                // 通过反射获取属性值
                 String fieldName = declaredFields[i].getName();
                 String getMethodName="get"+fieldName.substring(0,1).toUpperCase()+fieldName.substring(1);
                 Method declaredMethod = t.getClass().getDeclaredMethod(getMethodName);
+                //执行方法
                 Object fieldValue = declaredMethod.invoke(t);
-                if(fieldValue instanceof Integer){
-                    row.createCell(i).setCellValue(Integer.valueOf(fieldValue.toString()));
-                }else if(fieldValue instanceof Double){
-                    row.createCell(i).setCellValue(Double.valueOf(fieldValue.toString()));
-                }else if(fieldValue instanceof Date){
-                    row.createCell(i).setCellValue(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(fieldValue));
+                //判断是否为空
+                if(fieldValue!=null &&  !"".equals(fieldValue)){
+                    //判断属性值类型
+                    if(fieldValue instanceof Integer){
+                        row.createCell(i).setCellValue(Integer.valueOf(fieldValue.toString()));
+                    }else if(fieldValue instanceof Double){
+                        row.createCell(i).setCellValue(Double.valueOf(fieldValue.toString()));
+                    }else if(fieldValue instanceof Date){
+                        row.createCell(i).setCellValue(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(fieldValue));
+                    }else {
+                        row.createCell(i).setCellValue(fieldValue.toString());
+                    }
                 }else {
-                    row.createCell(i).setCellValue(fieldValue.toString());
+                    row.createCell(i).setCellValue("");
                 }
 
             }
         }
-
+//       自动设置列宽
         for (int i = 0; i < columnNames.length; i++) {
             sheet.autoSizeColumn(i);
-            sheet.setColumnWidth(i,sheet.getColumnWidth(i)*17/10);
+//            sheet.setColumnWidth(i,sheet.getColumnWidth(i)*17/10);
         }
-        workbook.write(new FileOutputStream(new File("/Users/rose/IdeaProjects/java-study/smalltools/ChangeData.xls")));
 
-        workbook.close();
+        return workbook;
+
     }
 }
--- smalltools/src/main/java/excell/Test.java
+++ smalltools/src/main/java/excell/Test.java
@@ -1,6 +1,11 @@
 package excell;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -12,21 +17,24 @@ import java.util.List;
  */
 public class Test {
     public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, IOException {
-      /*  PojoA pojoA=new PojoA();
+        PojoA pojoA=new PojoA();
         pojoA.setName("a");
         pojoA.setNum(1);
         pojoA.setPrice(1.1234);
         List<PojoA> lista=new ArrayList<>();
         lista.add(pojoA);
-        new ExcelUtil().setExcel(new String[]{"名称","数量","价格"},lista);
-*/
-        PojoB pojoB=new PojoB();
-        pojoB.setUserName("aa");
-        pojoB.setAge(1);
-        pojoB.setBirthday(new Date());
 
+        PojoB pojoB=new PojoB();
+        pojoB.setUserName("b");
+        pojoB.setAge(2);
+        pojoB.setBirthday(new Date());
         List<PojoB> listb=new ArrayList<>();
         listb.add(pojoB);
-        new ExcelUtil().setExcel("pojoB",new String[]{"名称","年龄","时间"},listb);
+
+        HSSFWorkbook workbooka = new ExcelUtil().setExcel("pojoA", new String[]{"名称", "数量", "价格"}, lista);
+        HSSFWorkbook workbookb = new ExcelUtil().setExcel("pojoB", new String[]{"名称", "年龄", "时间"}, listb);
+
+        workbooka.write(new FileOutputStream(new File("/Users/rose/IdeaProjects/java-study/smalltools/pojoA.xls")));
+        workbookb.write(new FileOutputStream(new File("/Users/rose/IdeaProjects/java-study/smalltools/pojoB.xls")));
     }
 }
